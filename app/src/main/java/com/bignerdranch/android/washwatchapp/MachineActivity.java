f2eb682867653f569f14b0bba27bfdf772f9d870
Akasha Archer <Akasha@LLD-IMAC.home>
2017-05-09 00:50:46 -0400

    machine status page setup

---
+++ app/src/main/java/com/bignerdranch/android/washwatchapp/MachineActivity.java
@@ -0,0 +1,19 @@
+package com.bignerdranch.android.washwatchapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Akasha on 5/9/17.
+ */
+
+public class MachineActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine);
+    }
+
+
+
+}
--- app/src/main/java/com/bignerdranch/android/washwatchapp/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/washwatchapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.bignerdranch.android.washwatchapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStatusBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStatusBtn = (Button) findViewById(R.id.status_btn);
+
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MachineActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
