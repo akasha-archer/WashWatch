267a69ea811802710a28c7a46c954465621ee60a
Akasha Archer <Akasha@LLD-IMAC.home>
2017-08-01 17:34:18 -0400

    progress

--- app/src/main/java/com/bignerdranch/android/washwatchapp/MachineActivity.java
+++ app/src/main/java/com/bignerdranch/android/washwatchapp/MachineActivity.java
@@ -1,18 +1,76 @@
 package com.bignerdranch.android.washwatchapp;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Akasha on 5/9/17.
  */
 
-public class MachineActivity extends AppCompatActivity {
+public class MachineActivity extends AppCompatActivity implements WashDialogFragment.WashDialogListener {
+
+    String diaTitle = "SELECT SOMETHING";
+    WashInProgress washInProgress;
+    String machineNum = WashDialogFragment.machineNumberSelection;
+    String washCycle =  WashDialogFragment.washCycleSelection;
+    String tenantPhone = WashDialogFragment.tenantPhoneNumber;
+
+    private static final String DIALOG_TITLE = "SELECT SOMETHING";
+
+    @BindView(R.id.selectMachine) Button selectMachine;
+    @BindView(R.id.machineOneClock) TextView machOneClock;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machine);
+
+        ButterKnife.bind(this);
+
+        selectMachine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               showDialog();
+            }
+        });
+
+        washInProgress = new WashInProgress(machineNum, washCycle, tenantPhone);
     }
+
+
+    void showDialog() {
+        FragmentManager manager = getSupportFragmentManager();
+        DialogFragment newFragment = new WashDialogFragment();
+        newFragment.show(manager, DIALOG_TITLE);
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
+        Log.i("FragmentAlertDialog", "Positive click!");
+        Toast.makeText(getApplicationContext(), WashDialogFragment.machineNumberSelection, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
+        Log.i("FragmentAlertDialog", "Negative click!");
+        Toast.makeText(getApplicationContext(), "Transaction Cancelled", Toast.LENGTH_SHORT).show();
+
+    }
+
+
 
 
 
---
+++ app/src/main/java/com/bignerdranch/android/washwatchapp/WashInProgress.java
@@ -0,0 +1,44 @@
+package com.bignerdranch.android.washwatchapp;
+
+/**
+ * Created by Akasha on 5/11/17.
+ */
+
+public class WashInProgress {
+
+    private String machineNumber;
+    private String washCycle;
+    private String tenantPhone;
+
+    public WashInProgress(String machineNumber, String washCycle, String tenantPhone) {
+        this.machineNumber = machineNumber;
+        this.washCycle = washCycle;
+        this.tenantPhone = tenantPhone;
+    }
+
+
+    public String getMachineNumber() {
+        return machineNumber;
+    }
+
+    public void setMachineNumber(String machineNumber) {
+        this.machineNumber = machineNumber;
+    }
+
+    public String getWashCycle() {
+        return washCycle;
+    }
+
+    public void setWashCycle(String washCycle) {
+        this.washCycle = washCycle;
+    }
+
+    public String getTenantPhone() {
+        return tenantPhone;
+    }
+
+    public void setTenantPhone(String tenantPhone) {
+        this.tenantPhone = tenantPhone;
+    }
+
+}
