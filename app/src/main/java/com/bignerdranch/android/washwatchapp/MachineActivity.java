ed4e5b54d6b17f78592646e9244a3140594661c7
Akasha Archer <Akasha@LLD-IMAC.home>
2017-08-02 02:10:35 -0400

    changes for the night

--- app/src/main/java/com/bignerdranch/android/washwatchapp/MachineActivity.java
+++ app/src/main/java/com/bignerdranch/android/washwatchapp/MachineActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.washwatchapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -217,6 +218,26 @@ public class MachineActivity extends AppCompatActivity implements WashDialogFrag
         }
 
     }
+
+
+//    public void setRegularTimer() {
+//        CountDownTimer regularCycleTimer = new CountDownTimer(2100000, 1000) {
+//            @Override
+//            public void onTick(long millisUntilFinished) {
+//
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//
+//            }
+//        }
+//
+//
+//    }
+
+
+
 }
 
 
